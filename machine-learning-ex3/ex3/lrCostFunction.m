c038551b92fb5f7f33b1766f4ea15cb41c3c95f4
Jefferson Song <jeffersonsong@yahoo.com>
2018-02-04 09:59:53 -0500

    Implement lrCostFunction.

--- machine-learning-ex3/ex3/lrCostFunction.m
+++ machine-learning-ex3/ex3/lrCostFunction.m
@@ -36,14 +36,13 @@ grad = zeros(size(theta));
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
 
+z = X * theta;
+h = sigmoid(z);
 
-
-
-
-
-
-
-
+theta2 = theta(:);
+theta2(1) = 0;
+J = 1 / m * (-y'* log(h) - (1 - y)' * log(1- h)) + lambda / (2*m) * (theta2' * theta2);
+grad = 1 / m * (X' * (h - y)) + lambda / m * theta2;
 
 % =============================================================
 
